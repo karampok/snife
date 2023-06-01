caaf7d6b7dab5cb0a1d489965a0123456eb44bf3
karampok <karampok@gmail.com>
2023-06-01 14:36:51 +0200

    add podman and echo workdir in the infra image

--- infra/start-ts.sh
+++ infra/start-ts.sh
@@ -1,30 +1,36 @@
 ##! /bin/bash
 set -euo pipefail
 
-echo 'Starting up tailscale...'
+echo "WORKDIR= $(pwd)"
+FILE=.ts-auth
+if test -f "$FILE"; then
+  echo 'tsed...'
 
-tailscaled 1>/tmp/tsed.logs 2>&1  &
-sleep 5
-if [ ! -S /var/run/tailscale/tailscaled.sock ]; then
-    echo "tailscaled.sock does not exist. exit!"
-    exit 1
+  tailscaled 1>/tmp/tsed.logs 2>&1 &
+  sleep 10
+  if [ ! -S /var/run/tailscale/tailscaled.sock ]; then
+      echo "tailscaled.sock does not exist. exit!"
+      exit 1
+  fi
+
+  ROUTES=$(ip route list proto kernel | cut -d " " -f1 |tr '\n' ','| sed -e 's/,$/\n/')
+  HOST=$(cat /proc/sys/kernel/hostname)
+
+  #TODO: ipv6
+
+  until tailscale up \
+      --authkey="$(cat .ts-auth)" \
+      --ssh --hostname="${HOST}" \
+      --advertise-routes "$ROUTES" \
+      --advertise-tags "tag:unsafe" 1>/tmp/tsup.logs 2>&1 
+  do
+      sleep 0.1
+  done
+  echo "mosh root@${HOST} -- tmux attach -t 0 -d"
+  echo "mutagen sync create --name=${HOST} (pwd) root@${HOST}:/workdir -i=bin --ignore-vcs"
+  echo "--advertise-routes $ROUTES"
 fi
 
-ROUTES=$(ip route list proto kernel | cut -d " " -f1 |tr '\n' ','| sed -e 's/,$/\n/')
-HOST=$(cat /proc/sys/kernel/hostname)
-
-#TODO: ipv6
-
-until tailscale up \
-    --authkey="${TS_AUTH}" \
-    --ssh --hostname="${HOST}" \
-    --advertise-routes "$ROUTES" \
-    --advertise-tags "tag:unsafe" 1>/tmp/tsup.logs 2>&1 
-do
-    sleep 0.1
-done
-
 mutagen-agent install
-echo "mosh root@workstation -- tmux attach -t 0 -d"
 
 trap : TERM INT; sleep infinity & wait
